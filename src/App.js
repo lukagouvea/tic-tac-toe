e6871dcf818bc022168a6dffa7e521157736eb1a
Luka <luka.gouvea0@gmail.com>
2023-06-27 18:31:02 -0300

    Tic Tac Toe

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, {useState} from "react"
+import './App.css';
+
+
+import {Board} from "./components/Board";
+import {ScoreBoard} from "./components/ScoreBoard"
+import {ResetButton} from "./components/ResetButton"
+
+function App() {
+
+
+  const WIN_CONDITIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  const[board, setBoard] = useState(Array(9).fill(null));
+  const[xPlaying, setXPlaying] = useState(true);
+  const[score, setScore] = useState({xScore: 0, oScore: 0})
+  const[gameOver, setGameOver] = useState(false);
+
+  const handleBoxClick = (boxIdx) => { 
+    const updatedBoard = board.map((value, idx) => {
+    if(idx === boxIdx) {
+      return xPlaying ? "X" : "O";
+    } else {
+      return value;
+    }
+    });
+
+    const winner = checkWinner(updatedBoard);
+
+    if(winner){
+      if(winner === "O"){
+        let{oScore} = score;
+        oScore += 1
+        setScore({...score, oScore})
+      } else {
+        let{xScore} = score;
+        xScore += 1
+        setScore({...score, xScore})
+      }
+    }
+
+    
+
+    setXPlaying(!xPlaying);
+
+    setBoard(updatedBoard);
+  };
+
+  const checkWinner = (board) => {
+    for(let i = 0; i < WIN_CONDITIONS.length;i++){
+      const [x,y,z] = WIN_CONDITIONS[i]
+
+      if(board[x] && board[x] === board[y] && board[y] === board[z]){
+        setGameOver(true);
+        return board[x];
+      }
+    }
+  }
+  
+
+
+  const resetBoard = () => {
+    setGameOver(false);
+    setBoard(Array(9).fill(null))
+  }
+
+  return(
+    <div className="App">
+      
+      <ScoreBoard scores={score} xPlaying={xPlaying}/>
+      <Board board={board} onClick={gameOver ? resetBoard : handleBoxClick}/>
+      <ResetButton resetBoard={resetBoard} />
+      
+    </div>
+    
+  );
+  
+};
+
+export default App;
